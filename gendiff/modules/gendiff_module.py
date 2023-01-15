e5b6a4b9f95e59db9c3b475a43f1be6e1d8b9ef1
Vladislav25 <vladkhleborodov@gmail.com>
2023-01-15 12:55:59 +0300

    add yml files logic

--- gendiff/modules/gendiff_module.py
+++ gendiff/modules/gendiff_module.py
@@ -1,4 +1,6 @@
 import json
+import yaml
+from yaml import FullLoader
 import itertools
 
 
@@ -19,9 +21,18 @@ def making_list(dict_1, dict_2):
     return new_list
 
 
+def open_file(file_1, file_2):
+    if file_1.endswith('json'):
+        dict_1 = json.load(open(file_1))
+        dict_2 = json.load(open(file_2))
+    else:
+        dict_1 = yaml.load(open(file_1), Loader=FullLoader)
+        dict_2 = yaml.load(open(file_2), Loader=FullLoader)
+    return dict_1, dict_2
+
+
 def generate_diff(file_1, file_2):
-    dict_1 = json.load(open(file_1))
-    dict_2 = json.load(open(file_2))
+    dict_1, dict_2 = open_file(file_1, file_2)
     sorted_dict_1 = dict(sorted(dict_1.items(), key=lambda x: x[0]))
     sorted_dict_2 = dict(sorted(dict_2.items(), key=lambda x: x[0]))
     new_list = making_list(sorted_dict_1, sorted_dict_2)
--- tests/test_flat.py
+++ tests/test_flat.py
@@ -1,8 +1,13 @@
-"""Float data testing"""
+"""Flat data testing"""
 
 from gendiff.modules.gendiff_module import generate_diff
 
-def test_flat():
+def test_flat_json():
     fixture = open('tests/fixtures/flat_fixture.txt')
     result = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
+    assert result == fixture.read()
+    
+def test_flat_yml():
+    fixture = open('tests/fixtures/flat_fixture.txt')
+    result = generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml')
     assert result == fixture.read()
