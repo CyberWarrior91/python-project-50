d836c8b93631bd27c755b2f4b4066509bad64e17
Vladislav25 <vladkhleborodov@gmail.com>
2023-03-12 18:30:32 +0300

    fix linter issues

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -6,7 +6,7 @@ def test_parse_module():
     yml_file = parse(open('tests/fixtures/file2.yml'), 'yml')
     data_type = type(json_file)
     assert data_type == dict
-    assert json_file == {'host': 'hexlet.io', 'timeout': 50, 
+    assert json_file == {'host': 'hexlet.io', 'timeout': 50,
                          'proxy': '123.234.53.22', 'follow': False}
     assert yml_file == {'host': 'hexlet.io', 'timeout': 20,
-                         'verbose': True}
+                        'verbose': True}
